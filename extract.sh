23f70798cfdef10801150537eea06883fd1bf6ae
eedaniel12 <144091036+eedaniel12@users.noreply.github.com>
2024-09-25 23:56:34 +0800

    Delete extract.sh

--- extract.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 下载文件并提取域名、IPv4和IPv6地址，保存到iptv_address.txt
-curl -s https://gitee.com/sunhaoyuan55/tool/raw/ceshi/ipv4.m3u | \
-grep -Eo '((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)' | \
-sort -u | \
-sed 's/$/\/32,no-resolve/' > iptv_address.txt
